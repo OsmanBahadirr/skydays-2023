ee06e93a45d28a9d1482c6295850b2c7e4f482c5
Ahmet MEYDAN <asmeydan0816@gmail.com>
2023-03-17 16:37:23 +0300

    last page updated

--- components/HomePage.js
+++ components/HomePage.js
@@ -3,7 +3,6 @@ import React from "react";
 const HomePage = () => {
   return (
     <div className="min-h-screen home flex flex-col items-center justify-between text-white pt-10 sm:pt-16 md:pt-24 snap-start">
-
       <div className=" flex flex-col items-end justify-center  text-4xl sm:text-8xl md:text-[7rem] font-extrabold relative home-page_titlebox py-5 mt-10 md:mt-20">
         <div className=" z-10 home-page_title w-full h-full flex items-center justify-center">
           SKYDAYS
@@ -15,11 +14,20 @@ const HomePage = () => {
 
       <div className=" w-full flex justify-end pr-10">
         <div className=" bg-[#00A99D] p-2 md:p-6 pr-10 md:pr-40 text-sm md:text-base">
-          25 NİSAN - YILDIZ TEKNİK ÜNİVERSİTESİ  TARİHİ HAMAM
+          25 NİSAN - YILDIZ TEKNİK ÜNİVERSİTESİ TARİHİ HAMAM
         </div>
       </div>
 
-      <div className=" mb-8">LOGO</div>
+      <div className=" mb-8 flex gap-5 text-lg md:text-4xl font-semibold md:font-bold">
+        <div className=" flex gap-2">
+          <div>*</div>
+          <div>YTÜ</div>
+        </div>
+        <div className=" flex gap-2">
+          <div>ROKET</div>
+          <div>SKYLAB</div>
+        </div>
+      </div>
     </div>
   );
 };
--- components/LastPage.js
+++ components/LastPage.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const LastPage = () => {
   return (
-    <div className=' h-screen bg-black snap-start'>
-        
-    </div>
-  )
-}
+    <div className=" h-screen bg-black snap-start flex flex-col justify-evenly items-center text-white/50">
+      <div className="  font-extrabold text-4xl sm:text-6xl md:text-8xl lg:text-9xl">
+        SKYDAYS
+      </div>
 
-export default LastPage
+      <div className=" flex flex-col items-center font-sans sm:text-lg md:text-2xl">
+        <div className=" flex gap-5 items-center">
+          <div>05554443322</div>
+          <span className=" w-2 h-2 rounded-full bg-white/50"></span>
+          <div>05554443322</div>
+        </div>
+
+        <div className=" text-center">
+          Davutpaşa Mah. Davutpaşa Caddesi 34220, Esenler / İSTANBUL
+        </div>
+      </div>
+
+      <div className=" mb-8 flex gap-5 text-lg md:text-4xl font-semibold md:font-bold">
+        <div className=" flex gap-2">
+          <div>*</div>
+          <div>YTÜ</div>
+        </div>
+        <div className=" flex gap-2">
+          <div>ROKET</div>
+          <div>SKYLAB</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LastPage;
