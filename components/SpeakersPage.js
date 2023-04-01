804cf3e291db32f2d255fb393570011a3d3107db
Ahmet MEYDAN <asmeydan0816@gmail.com>
2023-04-02 00:08:00 +0300

    speakers page updated

--- components/SpeakersPage.js
+++ components/SpeakersPage.js
@@ -9,30 +9,22 @@ const SpeakersPage = () => {
         KONUŞMACILAR
       </div>
 
-      <div className=' grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-10'>
+      <div className=' grid grid-cols-2 md:grid-cols-4 gap-5 md:gap-10'>
 
-        <div>
-          <Image className=' w-20 md:w-[150px] border border-[#7BE9D3]' alt="konuşmacı" src={speakerImage} width={150}  />
-          <div className=' text-sm md:text-base font-normal md:font-medium mt-2'>????? ?????</div>
-          <div className=' text-xs text-zinc-600'>??????????.???</div>
+        <div className='w-20 text-6xl md:text-9xl font-bold md:w-[150px] border border-[#7BE9D3] flex justify-center items-center bg-black '>
+          ?
         </div>
 
-        <div>
-          <Image className=' w-20 md:w-[150px] border border-[#7BE9D3]' alt="konuşmacı" src={speakerImage} width={150}  />
-          <div className=' text-sm md:text-base font-normal md:font-medium mt-2'>????? ?????</div>
-          <div className=' text-xs text-zinc-600'>??????????.???</div>
+        <div className='w-20 text-6xl md:text-9xl font-bold md:w-[150px] border border-[#7BE9D3] flex justify-center items-center bg-black'>
+          ?
         </div>
 
-        <div>
-          <Image className=' w-20 md:w-[150px] border border-[#7BE9D3]' alt="konuşmacı" src={speakerImage} width={150}   />
-          <div className=' text-sm md:text-base font-normal md:font-medium mt-2'>????? ?????</div>
-          <div className=' text-xs text-zinc-600'>??????????.???</div>
+        <div className='w-20 text-6xl md:text-9xl font-bold md:w-[150px] border border-[#7BE9D3] flex justify-center items-center bg-black'>
+          ?
         </div>
 
-        <div>
-          <Image className=' w-20 md:w-[150px] border border-[#7BE9D3]' alt="konuşmacı" src={speakerImage} width={150}   />
-          <div className=' text-sm md:text-base font-normal md:font-medium mt-2'>????? ?????</div>
-          <div className=' text-xs text-zinc-600'>??????????.???</div>
+        <div className='w-20 text-6xl md:text-9xl font-bold md:w-[150px] border border-[#7BE9D3] flex justify-center items-center bg-black'>
+          ?
         </div>
 
       </div>
